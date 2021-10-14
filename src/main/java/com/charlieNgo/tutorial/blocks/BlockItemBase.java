16abcdb2868dd54143fe669412944a1c21bd89a5
CharlieNeggo <charliengo1112@Gmail.com>
2021-10-13 21:02:52 -0400

    Initial commit

---
+++ src/main/java/com/charlieNgo/tutorial/blocks/BlockItemBase.java
@@ -0,0 +1,13 @@
+package com.charlieNgo.tutorial.blocks;
+
+import com.charlieNgo.tutorial.Tutorial;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class BlockItemBase extends BlockItem {
+
+    public BlockItemBase(Block block) {
+        super(block, new Item.Properties().group(Tutorial.TAB));
+    }
+}
