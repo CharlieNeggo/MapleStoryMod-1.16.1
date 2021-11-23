b095f88903bb5447b595bbb5907807b7be9a782a
CharlieNeggo <charliengo1112@Gmail.com>
2021-11-22 22:52:47 -0500

    Sup

---
+++ src/main/java/com/charlieNgo/rubyoremod/items/ItemBase.java
@@ -0,0 +1,11 @@
+package com.charlieNgo.rubyoremod.items;
+
+import com.charlieNgo.rubyoremod.RubyOreMod;
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item{
+
+    public ItemBase() {
+        super(new Item.Properties().group(RubyOreMod.TAB));
+    }
+}
---
+++ src/main/java/com/charlieNgo/rubyoremod/util/RegistryHandler.java
@@ -0,0 +1,50 @@
+package com.charlieNgo.rubyoremod.util;
+
+import com.charlieNgo.rubyoremod.RubyOreMod;
+import com.charlieNgo.rubyoremod.blocks.BlockItemBase;
+import com.charlieNgo.rubyoremod.blocks.RubyBlock;
+import com.charlieNgo.rubyoremod.items.ItemBase;
+import com.charlieNgo.rubyoremod.tools.ModItemTier;
+import net.minecraft.block.Block;
+import net.minecraft.item.*;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, RubyOreMod.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, RubyOreMod.MOD_ID);
+
+    public static void init() {
+
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    // Items
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    // Tools
+    // Sword
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () -> new SwordItem(ModItemTier.RUBY, 6, -1.5F
+    , new Item.Properties().group(RubyOreMod.TAB)));
+
+    //Pickaxe
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () -> new PickaxeItem(ModItemTier.RUBY, 4, -1.7F
+            , new Item.Properties().group(RubyOreMod.TAB)));
+
+    //Axe
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () -> new AxeItem(ModItemTier.RUBY, 8, -2.0F
+            , new Item.Properties().group(RubyOreMod.TAB)));
+
+    //Shovel
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () -> new ShovelItem(ModItemTier.RUBY, 3, -1.9F
+            , new Item.Properties().group(RubyOreMod.TAB)));
+
+    //Blocks
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+
+    //Block Items
+    public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
+}
